c005e5b90b6178c4af6ec87361cb24e723bc5f10
PerikoElmaki <pedro.sandoval.martinez@ces-vegamedia.es>
2024-04-22 12:40:56 +0200

    n

--- Triggers/Notas.sql
+++ Triggers/Notas.sql
@@ -36,3 +36,77 @@ INSERT INTO ASIGNATURA VALUES('BD','Bases de Datos',1,5),
 ('LM','Lenguaje de Marcas',2,5),
 ('DIW','Diseño de interfaces Web',2,9);
 
+INSERT INTO Alumno VALUES (0,'Pedro Martinez','1998/02/17');
+INSERT INTO Alumno VALUES (0,'Ana Fuentes', '1997-04-21');
+INSERT INTO Alumno VALUES (0,'Juan Salas','1998-11-12');
+INSERT INTO Alumno VALUES (0,'Carmen Pedroso', '1997-03-25');
+INSERT INTO Alumno VALUES (0,'Javier Buendia','1998-12-11');
+INSERT INTO Alumno VALUES (0,'Antonio Calvo','1998-02-22');
+-- Creamos variables que cuenten numero de aprobados y suspensos 
+
+SET @aprobados=0;
+set @suspensos=0;
+
+-- Trigger que cuente cuando insertes nota 
+
+DELIMITER $$
+CREATE TRIGGER evaluar_insert 
+AFTER INSERT ON Nota 
+FOR EACH ROW 
+BEGIN 
+	IF NEW.nota>=5 THEN 
+		SET @aprobados=@aprobados+1;
+	ELSE
+		SET @suspensos=@suspensos+1;
+	END IF;
+END $$
+DELIMITER ;
+
+INSERT INTO Nota VALUES (1,'BD',5);
+INSERT INTO Nota VALUES (2,'BD',8);
+INSERT INTO Nota VALUES (3,'BD',10);
+INSERT INTO Nota VALUES (4,'BD',3);
+INSERT INTO Nota VALUES (5,'BD',7);
+INSERT INTO Nota VALUES (6,'BD',6);
+INSERT INTO Nota VALUES (1,'SI',5);
+INSERT INTO Nota VALUES (2,'SI',8);
+INSERT INTO Nota VALUES (3,’SI’,10);
+INSERT INTO Nota VALUES (4,’SI’,3);
+INSERT INTO Nota VALUES (5,’SI’,7);
+INSERT INTO Nota VALUES (6,’SI’,6);
+
+-- TRigger que controle la modificacion 
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS evaluar_update $$
+CREATE TRIGGER evaluar_update
+BEFORE UPDATE ON Nota 
+FOR EACH ROW 
+BEGIN 
+	IF OLD.nota>=5 THEN 
+		IF NEW.nota<5 THEN 
+			SET @aprobados=@aprobados-1;
+			SET @suspensos=@suspensos+1;
+		END IF; 
+	ELSE 
+		IF NEW.nota>=5 THEN 
+			SET @aprobados=@aprobados+1;
+			SET @suspensos=@suspensos-1;
+		END IF; 
+	END IF;
+END $$
+DELIMITER ;
+
+UPDATE Nota
+SET nota=4
+WHERE codAlum=1;
+
+SELECT @aprobados;
+SELECT @suspensos;
+
+UPDATE Nota
+SET nota=nota+1
+WHERE codAlum=2;
+
+SELECT @aprobados;
+SELECT @suspensos;
